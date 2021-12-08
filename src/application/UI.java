39c45b821843d010d778ac764ad728fa07e889ad
Moises Weber <moisesweber01@gmail.com>
2021-12-08 16:19:51 -0300

    Piece move count

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,8 +57,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+			if (!chessMatch.getCheckMate()) {
+				System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+		}
+	} else {
+		System.out.println("CHACKMATE!");
+		System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
